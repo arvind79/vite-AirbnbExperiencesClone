224fb54ce1c73220223379495f8481eede2a9e9a
arvind79 <81362519+arvind79@users.noreply.github.com>
2023-04-16 17:49:14 +0530

    images path corrected

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,10 @@
 import React from "react"
+import AirbnbLogo from "../images/airbnb-logo.png"
 
 export default function Navbar() {
     return (
         <nav>
-            <img src="../images/airbnb-logo.png" className="nav--logo" />
+            <img src={AirbnbLogo} className="nav--logo" />
         </nav>
     )
 }
